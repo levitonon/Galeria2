64126d62cfa133991635216496e316b3389e7912
Levi Tonon <114413223+levitonon@users.noreply.github.com>
2025-02-04 14:51:49 -0300

    Comments

--- app/src/main/java/lisboa/tonon/levi/galeria/PhotoActivity.java
+++ app/src/main/java/lisboa/tonon/levi/galeria/PhotoActivity.java
@@ -27,6 +27,7 @@ import util.Util;
 public class PhotoActivity extends AppCompatActivity {
 
     String photoPath;
+    // Caminho completo da foto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,42 +39,63 @@ public class PhotoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Configura a Toolbar
         Toolbar toolbar = findViewById(R.id.tbPhoto);
         setSupportActionBar(toolbar);
+
+        // Pega a referência da ActionBar para habilitar o botão de retorno
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
         Intent i = getIntent();
+        // Extrai a string "photo_path" passada de outra tela
         photoPath = i.getStringExtra("photo_path");
+
         Util Utils = new Util();
+
+        // Para exibir a imagem, obtemos o bitmap correspondente ao caminho da foto.
         Bitmap bitmap = Utils.getBitmap(photoPath);
+
+        // Pega a referência do ImageView do layout e define o bitmap como conteúdo.
         ImageView imPhoto = findViewById(R.id.imPhoto);
         imPhoto.setImageBitmap(bitmap);
-
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Infla o menu específico desta tela (com opção de compartilhar).
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.photo_activity_tb, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.opShare:
+                // Se o usuário clicou na opção de compartilhar, chama sharePhoto().
                 sharePhoto();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
- }
- }
+        }
+    }
+
     void sharePhoto() {
-        // Codigo para cpmpartiilhar a foto
-        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this,
-                "trindade.daniel.galeria.fileprovider", new File(photoPath));
+        // Método responsável por compartilhar a foto armazenada em photoPath.
+        Uri photoUri = FileProvider.getUriForFile(
+                PhotoActivity.this,
+                "trindade.daniel.galeria.fileprovider",
+                new File(photoPath)
+        );
+
+        // Cria uma Intent de envio, anexa a foto e define o tipo de arquivo.
         Intent i = new Intent(Intent.ACTION_SEND);
         i.putExtra(Intent.EXTRA_STREAM, photoUri);
         i.setType("image/jpeg");
+
         startActivity(i);
-        }
+    }
 }
